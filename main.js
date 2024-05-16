2ebd0a29f587059f6d8bb891cbf95d9ad1cacbe2
DanielWebMap <Daniel.Schoenung@student.uibk.ac.at>
2024-05-16 18:10:43 +0200

    Winddate Stand und DOwnloadLink

--- main.js
+++ main.js
@@ -115,7 +115,9 @@ async function loadWind(url) {
     //Vorhersagezeitpunkt ermitteln
     let forecastDate = new Date(jsondata[0].header.refTime)
     forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime);
-    console.log(forecastDate);
+    //console.log(forecastDate);
+
+    document.querySelector("#forecast-date").innerHTML = `(<a href="${url}"> Stand ${forecastDate.toLocaleString()})`
 }
 
 loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json")
